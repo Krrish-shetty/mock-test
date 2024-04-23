5e8740d9fb84513f350fb37d3fbb7b6338f3e676
KRRISH DINESH SHETTY <93126029+Krrish-shetty@users.noreply.github.com>
2024-04-23 01:24:34 -0700

    Update kk.py

--- kk.py
+++ kk.py
@@ -1,4 +1,4 @@
-a = int(input("Enter the number"))
-b = int(input("Enter second number"))
+a = int(input("Enter the number = "))
+b = int(input("Enter second number = "))
 c = a + b
-print("The answer is " , c)
+print("The answer is =" , c)
